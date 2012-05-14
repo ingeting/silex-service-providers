ce02eb51b18f383674688d1c16f65178bc31de52
Inge Thorud <inge.thorud@gmail.com>
2012-05-14 23:11:20 +0200

    Version 0.1
    Signed-off-by: Inge Thorud <inge.thorud@gmail.com>

---
+++ lib/ingeting/Silex/Provider/HybridAuthServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ingeting\Silex\Provider;
+ 
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+ 
+class HybridAuthServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        /*$app['auth'] = $app->share(function () use ($app) {
+            return new HybridAuthWrapper();
+        });*/
+        
+        $app['auth'] = $app->share(function () use ($app) {
+            
+            require_once $app['hybridauth.class_file'];
+            
+         
+            $config = array(
+				"base_url" => $app['hybridauth.base_url'],  
+		
+				"providers" => array ( 
+					// openid providers
+					"Facebook" => array ( 
+						"enabled" => true,
+						"keys"    => array ( "id" => $app['fb.config.app_id'], "secret" => $app['fb.config.secret'] ),
+		
+						// A comma-separated list of permissions you want to request from the user. See the Facebook docs for a full list of available permissions: http://developers.facebook.com/docs/reference/api/permissions.
+						"scope"   => ",", 
+		
+						// The display context to show the authentication page. Options are: page, popup, iframe, touch and wap. Read the Facebook docs for more details: http://developers.facebook.com/docs/reference/dialogs#display. Default: page
+						"display" => "" 
+					)
+				),
+		
+				// if you want to enable logging, set 'debug_mode' to true  then provide a writable file by the web server on "debug_file"
+				"debug_mode" => true,
+		
+				"debug_file" => $app['hybridauth.config.log_file'],
+			);
+
+            return new \Hybrid_Auth($config);
+        });
+    }
+}
---
+++ lib/ingeting/Silex/Provider/ParisProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace ingeting\Silex\Provider;
+ 
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+ 
+//use Cs\Utils as u;
+     
+ 
+require_once __DIR__.'/../../../vendor/Idiorm/idiorm.php';
+require_once __DIR__.'/../../../vendor/Paris/paris.php';
+
+
+ 
+class ParisProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        $app['paris'] = $app->share(function () use ($app) {
+            \ORM::configure($app['paris.dsn']);
+            \ORM::configure('username', $app['paris.username']);
+            \ORM::configure('password', $app['paris.password']);
+            \ORM::configure('logging', true );
+            \ORM::configure('driver_options', array(
+                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+            ));
+ 
+            return new ParisWrapper();
+        });
+    }
+}
+ 
+class ParisWrapper
+{
+    public function getModel($modelName)
+    {
+        return \Model::factory($modelName);
+    }
+     
+    public function getLastQuery()
+    {
+        return \ORM::get_last_query();
+    }
+     
+    public function getQueryLog()
+    {
+        return \ORM::get_query_log();
+    }
+}
